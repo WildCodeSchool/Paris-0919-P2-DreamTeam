5feb673f4bdda4d3f9a82ebec4f43fd0833e0f75
Hkwolfx <bourlandlalexis@orange.fr>
2019-11-04 17:33:23 +0100

    Improved homepage, getting clean to receive components.

--- leviosound/src/components/Header.js
+++ leviosound/src/components/Header.js
@@ -1,20 +1,27 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import "./Header.css"
+import "./Header.css";
 
 const Header = () => {
-	return (
-	 	 <div className="Header-nav">
-					<div>
-						<ul className="header-menu">
-						<Link to="/"><li className="header-itemMenu">Home</li></Link>
-						<Link to="/weather"><li className="header-itemMenu">My Playlist</li></Link>
-						<Link to="/MyAlarm"><li className="header-itemMenu">My Alarm</li></Link>
-						</ul>
-					</div>
-	 	 </div>
-	)	
-}
+  return (
+    <div className="Header-nav">
+      <p className="titre">LEVIOSOUND</p>
+      <div>
+        <ul className="header-menu">
+          <Link to="/">
+            <li className="header-itemMenu">Home</li>
+          </Link>
+          <Link to="/weather">
+            <li className="header-itemMenu">My Playlist</li>
+          </Link>
+          <Link to="/MyAlarm">
+            <li className="header-itemMenu">My Alarm</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- leviosound/src/screen/Home.js
+++ leviosound/src/screen/Home.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Image from "./eau.jpg"
+
 import './Home.css';
+
+const styles = {
+  coversContainer: {
+    backgroundImage: `url(${Image})`
+  }
+};
 
 const Home = () => {
   return (
-    <div class="container">
+    <div class="container1">
       <div class="menu">HOME</div>
-      <div class="post">POST</div>
-      <div class="sidebar">SIDEBAR</div>
+      <div class="post" style={styles.coversContainer}> Album Covers </div>
+      <div class="sidebar">TODAY'S PLAYLIST</div>
+      <div class="sidebar">LYRICS</div>
+      <div class="sidebar">ADD NEWS SONGS</div>
       <div class="footer">FOOTER</div>
     </div>
   );
