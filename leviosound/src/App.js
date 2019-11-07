ce43f8d880b179cf7880b764430e62955bd1c2a0
Alexandre Bardot <bardotalexandre@gmail.com>
2019-11-07 11:04:54 +0100

    finition myalarm avant integration de ring

--- leviosound/src/App.js
+++ leviosound/src/App.js
@@ -8,19 +8,21 @@ import Header from "./components/Header"
 import Home from './screen/Home';
 import MyPlaylist from './screen/MyPlaylist';
 import MyAlarm from './screen/MyAlarm';
+import VideoContainer from './screen/VideoContainer'
 import SearchBar from './components/SearchBar';
 import './App.css';
 function App() {
   return (
     <div className="App">
+          <VideoContainer />
       <Header />
           
         <Switch>
           <Route exact path="/" component={Home}> Home
           <SearchBar />
           </Route>
-          <Route path="/weather" component={MyPlaylist} />
           <Route path="/MyAlarm" component={MyAlarm} />
+          <Route path="/weather" component={MyPlaylist} />
         </Switch>
     </div>
   );
