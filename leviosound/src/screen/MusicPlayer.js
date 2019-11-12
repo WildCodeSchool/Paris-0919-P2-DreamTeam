efea97fd947e537d99f322eed4393b1175e635d6
Hkwolfx <bourlandlalexis@orange.fr>
2019-11-12 15:38:46 +0100

    saving changes. global css of the music player

--- leviosound/src/screen/MusicPlayer.js
+++ leviosound/src/screen/MusicPlayer.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import MusicPlayer from 'react-responsive-music-player';
+import './burroak1.png'
+import './burroakrevenge.mp3'
 
 
 const playlist = [
     {
-      url: 'path/to/mp3',
-      cover: 'path/to/jpg',
-      title: 'Despacito',
+      url: 'https://audio.jukehost.co.uk/b9f1ebff74ddef689977dcb6013c27339f6244b6/7c923005dff',
+      cover: './burroak1.png',
+      title: 'Burroak - Revenge',
       artist: [
-        'Luis Fonsi',
-        'Daddy Yankee'
+        'Burr Oak',
       ]
     },
     {
@@ -21,10 +22,6 @@ const playlist = [
       ]
     }
   ]
-
-
-
-
 
 
 
